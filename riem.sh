42f6c44a16aaaa493ab1cdd93bb63c7e1e97a86f
CraicOverflow89 <craic.overflow@hotmail.com>
2019-11-22 12:12:27 +0000

    Started working on the riem script (bash/batch script that invokes the riem.shell code) that provides access to app skeleton builds and suchlike.

---
+++ riem.sh
@@ -0,0 +1,1 @@
+python -m riem.shell "$@" > /dev/null
---
+++ riem/shell.py
@@ -0,0 +1,39 @@
+import sys
+
+# Script Logic
+def invoke(args):
+
+	# Command: Help
+	def help():
+		print("Help")
+		# NOTE: list commands and args
+
+	# Command: Create
+	def create(args):
+		print("App Skeleton")
+		# NOTE: create dirs and files at cwd
+
+	# Command Map
+	commands = {
+		"create" : create,
+		"help" : help
+	}
+
+	# No Command
+	if len(args) == 0:
+		help()
+
+	# Parse Command
+	else:
+
+		# Valid Command
+		if args[0] in commands.keys():
+			commands[args[0]](args[1:])
+
+		# Invalid Command
+		else:
+			print("Command %s not recognised" % args[0])
+			help()
+
+# Invoke Script
+invoke(sys.argv[1:])
